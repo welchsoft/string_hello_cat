e3b1a3bce43bfd6d5b97f6275177aa8e72f6979e
wade welch <wade.c.welch@gmail.com>
2018-05-01 14:05:28 -0500

    added comments

--- stringcat.py
+++ stringcat.py
@@ -1,6 +1,9 @@
+#define function that takes first and last name and adds them to statement
 def helloMyDude(name1,name2):
     print("Hello, My name is " + name1 + ", " + name2)
 
+# take in first and last name from user
 firstname = input("please enter your first name ")
 lastname = input("please enter your last name ")
+#call function and pass it the names
 helloMyDude(firstname, lastname)
