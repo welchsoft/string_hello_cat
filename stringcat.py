b83b6adf7b55fb469237859e6be870c5c3fc24a1
wade welch <wade.c.welch@gmail.com>
2018-05-01 12:23:46 -0500

    takes your name and says hello

---
+++ stringcat.py
@@ -0,0 +1,6 @@
+def helloMyDude(name1,name2):
+    print("Hello, My name is " + name1 + ", " + name2)
+
+firstname = input("please enter your first name ")
+lastname = input("please enter your last name ")
+helloMyDude(firstname, lastname)
